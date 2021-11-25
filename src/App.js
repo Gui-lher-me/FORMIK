fda330cea555ddd5558ba32237fa5016440e013a
Guilherme G Silva <gui_g88@hotmail.com>
2021-11-25 16:32:33 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import * as yup from "yup"
+import {useFormik} from "formik"
 
 function App() {
+  const {handleChange, handleSubmit, values, errors, touched, handleBlur} = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    onSubmit: ({email, password}) => console.log(email, password),
+    validationSchema: yup.object({
+      email: yup.string().max(10, "Email must be shorter than 10 characters").required("Required"),
+      password: yup.string().min(6, "Password should be longer than 6 characters").required(),
+    })
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <form onSubmit={handleSubmit} className="">
+      <div>
+        <label>Email</label>
+        <input name="email" value={values.email} onBlur={handleBlur} onChange={handleChange} />
+        {touched.email && errors.email && <span style={{color: "red"}}>{errors.email}</span>}
+      </div>
+      <div>
+        <label>Senha</label>
+        <input name="password" value={values.password} onBlur={handleBlur} onChange={handleChange} />
+        {touched.password && errors.password && <span style={{color: "red"}}>{errors.password}</span>}
+      </div>
+      <input type="submit" value="Send" />
+    </form>
+  )
 }
 
-export default App;
+export default App
+
